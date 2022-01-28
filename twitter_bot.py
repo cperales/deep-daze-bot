f98ff2dff0b0e1766bedc3eda3534100653c186a
Carlos Perales <sir.perales@gmail.com>
2022-01-28 11:47:05 +0100

    API and read credentials

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,13 +1,20 @@
 import tweepy
 import configparser
 
-config = configparser.read('config.ini')
-api_key = config.get('API_KEY')
-api_key_secret = config.get('API_KEY_SECRET')
-bearer_token = config.get('BEARER_TOKEN')
-auth = tweepy.Client(bearer_token=bearer_token)
+config = configparser.ConfigParser()
+config.read_file(open('config.ini'))
+api_key = config.get('TWITTER', 'API_KEY')
+api_key_secret = config.get('TWITTER', 'API_KEY_SECRET')
+bearer_token = open('bearer_token.txt').readlines()[0]
+auth = tweepy.OAuth2BearerHandler(bearer_token=bearer_token)
 
 # Create API object
 api = tweepy.API(auth)
 
-api = tweepy.API(auth)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+
+api.update_status("Test tweet from Deep Daze Bot. Hello world!")
