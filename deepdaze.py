7f60fefbb6960b961fbcdd1dd521a73f0e648ca3
Carlos Perales <sir.perales@gmail.com>
2022-02-02 00:40:09 +0100

    api just used in the on_status

--- deepdaze.py
+++ deepdaze.py
@@ -1,16 +1,15 @@
 from deep_daze import Imagine
 
 imagine = Imagine(
-    text='a blue orb',
-    lr=1e-4,
+    text='a dog eating on the lawn',
     image_width=256,
-    num_layers=12,
-    batch_size=2,
-    epochs=1,
+    num_layers=24,
+    batch_size=4,
+    epochs=3,
     iterations=800,
-    save_progress=False,
+    save_progress=True,
     open_folder=False,
-    gradient_accumulate_every=2
+    gradient_accumulate_every=1
 )
 
 imagine()
--- twitter_bot.py
+++ twitter_bot.py
@@ -15,17 +15,6 @@ access_token = config.get('TWITTER', 'ACCESS_TOKEN')
 access_secret = config.get('TWITTER', 'ACCESS_TOKEN_SECRET')
 bearer_token = open('bearer_token.txt').readlines()[0]
 
-# Construct the API instance for replying tweets
-auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
-
-
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    raise Exception("Error during authentication")
 
 
 class MyStreamListener(tweepy.Stream):
@@ -44,22 +33,54 @@ class MyStreamListener(tweepy.Stream):
 
         # Imagine
         self.dream(text_wo_name)
+
+        # Reply
+        # Construct the API instance for replying tweets
+        auth = tweepy.OAuthHandler(api_key, api_secret)
+        auth.set_access_token(access_token, access_secret)
+        api = tweepy.API(auth)
+
+        try:
+            api.verify_credentials()
+            print("Authentication OK")
+        except:
+            raise Exception("Error during authentication")
+
         api.update_status_with_media(filename=filename,
                                      status='.@' + screen_name + ' Your words were dreamt',
                                      in_reply_to_status_id=tweet_id)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # Reply
+            # Construct the API instance for replying tweets
+            auth = tweepy.OAuthHandler(api_key, api_secret)
+            auth.set_access_token(access_token, access_secret)
+            api = tweepy.API(auth)
+
+            try:
+                api.verify_credentials()
+                print("Authentication OK")
+            except:
+                raise Exception("Error during authentication")
+
+            api.update_status(status='@Relampague bot goes offline')
+
+            #returning False in on_data disconnects the stream
+            return False
 
     @staticmethod
     def dream(text):
         imagine = Imagine(
             text=text,
-            image_width=512,
-            num_layers=8,
-            batch_size=1,
-            epochs=1,
-            iterations=1000,
-            save_progress=False,
+            image_width=256,
+            num_layers=24,
+            batch_size=4,
+            epochs=3,
+            iterations=800,
+            save_progress=True,
             open_folder=False,
-            gradient_accumulate_every=4
+            gradient_accumulate_every=1
         )
         imagine()
         del imagine
